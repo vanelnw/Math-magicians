0294c02d54ffe96f969fc7ce4116aafa72a8b2da
vanelnw <vanelnwaba@gmail.com>
2022-12-01 07:30:15 +0100

    modifier stucture and handle event

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,3 +1,8 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-loop-func */
+/* eslint-disable spaced-comment */
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/button-has-type */
 import { Component } from 'react';
 import './Calculator.css';
@@ -6,45 +11,58 @@ class Calculator extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
-    createDigits = () => {
-      const digits = [];
-      let iterate = 9;
-      while (iterate > 0) {
-        digits.push(<button>{iterate}</button>);
-        iterate -= 1;
-      }
-      return digits;
-    }
-
-    render() {
-      return (
-        <div className="calculator">
-          <div className="display">
-            <span>0</span>
-          </div>
-          <div className="digitContainer">
-            <div className="digits">
-              <button>%</button>
-              <button>+/-</button>
-              <button>AC</button>
-              {this.createDigits()}
-              <button>.</button>
-              <button>0</button>
-            </div>
-            <div className="operators">
-              <button>÷</button>
-              <button>x</button>
-              <button>-</button>
-              <button>+</button>
-              <button>=</button>
-            </div>
-          </div>
-        </div>
+  createDigits = () => {
+    // eslint-disable-next-line react/prop-types
+    const { makeCalculation } = this.props;
+    const digits = [];
+    for (let i = 9; i > 0; i--) {
+      digits.push(
+        <button key={i} onClick={() => makeCalculation(`${i}`)}>
+          {i}
+        </button>,
       );
     }
+    return digits;
+  };
+
+  render() {
+    const oparations = ['÷', 'x', '-', '+', '='];
+    const { makeCalculation, output } = this.props;
+    let expression = output.total || 0;
+    if (!output.total && output.next) expression = '';
+    const expression3 = output.next || '';
+    const expression2 = output.operation || '';
+
+    return (
+      <div className="calculator">
+        <div className="display">
+          <span>{expression + expression2 + expression3}</span>
+        </div>
+        <div className="digitContainer">
+          <div className="digits">
+            <button onClick={() => makeCalculation('%')}>%</button>
+            <button onClick={() => makeCalculation('+/-')}>+/-</button>
+            <button onClick={() => makeCalculation('AC')}>AC</button>
+            {this.createDigits()}
+            <button onClick={() => makeCalculation('.')}>.</button>
+            <button onClick={() => makeCalculation('0')}>0</button>
+          </div>
+          <div className="operators">
+            {oparations.map((item) => (
+              <button
+                key={oparations.indexOf(item)}
+                onClick={() => makeCalculation(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
