3dd10160ec8e1b0021d8d98c56db50dd223789ee
vanelnw <vanelnwaba@gmail.com>
2022-12-01 07:31:50 +0100

    modifie as class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+/* eslint-disable react/no-unused-state */
+import { Component } from 'react';
 import './App.css';
 // eslint-disable-next-line import/extensions
 import Calculator from './components/Calculator.js';
+import calculate from './logic/calculate';
 
-function App() {
-  return <div className="App"><Calculator /></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  makeCalculation = (buttonName) => {
+    const obj = this.state;
+    const output = calculate(obj, buttonName);
+    this.setState(output);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator makeCalculation={this.makeCalculation} output={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
